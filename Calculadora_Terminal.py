ac95398b0e09aeaa63960ce9b894ee1128970d0f
Nathan Caliani <nathan.caliani09@gmail.com>
2025-04-24 17:20:14 -0300

    Calculadora_Terminal código completo

--- Calculadora_Terminal.py
+++ Calculadora_Terminal.py
@@ -1,2 +1,34 @@
 # Calculadora feita no terminal
 
+# função para limpar a tela, de forma gambiarra
+def limpatela():
+    print("\n" * 10)
+
+# deixa o programa ativo
+while True:
+    # para pegar explicar como funciona e pegar o input
+    print("+ para adição, - para subtração, * para vezes, / para divisão, ( ) para preferência.\n"
+          "Exemplo: 23*3+(10/4)-7\n"
+          "Digite a conta matématica que você quer fazer: ", end="")
+    conta = input()
+
+    # para calcular
+    try:
+        print(eval(conta))
+    except:
+        print("ERRO!")
+
+    # verificar se o usuário quer continuar
+    continuar = input("Deseja continuar [S/N]: ").upper().strip()
+    while continuar not in "SN":
+        print("Por favor, digite 'S' para Sim ou 'N' para Não.")
+        continuar = input("Deseja continuar [S/N]: ").upper().strip()
+
+    # limpa a tela do terminal
+    limpatela()
+
+    # para encerrar o programa
+    if continuar == "N":
+        print("Programa encerrado!")
+        break
+# FIM
